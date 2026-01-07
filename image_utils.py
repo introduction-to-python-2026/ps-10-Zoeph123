1f81ec4f75f2784396b15e2d5ad667d623a6af6b
Zoeph123 <zoe.philipson2@gmail.com>
2026-01-07 19:47:07 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -3,20 +3,6 @@ import numpy as np
 from scipy.signal import convolve2d
 
 def load_image(path):
-    pass # Replace the `pass` with your code
-
-def edge_detection(image):
-    pass # Replace the `pass` with your code
-
-
-
-
-
-from PIL import Image
-import numpy as np
-from scipy.signal import convolve2d
-
-def load_image(path):
     """
     טוענת תמונה מהנתיב הנתון וממירה אותה לגווני אפור (Grayscale)
     כדי שנוכל לבצע עליה חישובים מתמטיים בקלות.
