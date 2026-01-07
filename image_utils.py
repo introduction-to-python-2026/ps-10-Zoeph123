16b80cae5242ac0565fab171b431b16bde65c104
Zoeph123 <zoe.philipson2@gmail.com>
2026-01-07 20:32:45 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -10,10 +10,10 @@ def load_image(path):
 def edge_detection(image):
   gray_image = np.mean(image, axis=2)
   kernelY = np.array([[1, 1, 1],
-                      [0, 0, 0]
+                      [0, 0, 0],
                       [-1, -1, -1]])
   kernelX = np.array([[1, 0, -1],
-                      [1, 0, -1]
+                      [1, 0, -1],
                       [1, 0, -1]])
   edge_y = convolve2d(gray_image, kernelY, mode='same')
   edge_x = convolve2d(gray_image, kernelX, mode='same')
