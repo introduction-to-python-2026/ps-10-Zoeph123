96d0ad819b01b6cdda09cc1f30fcf07d1e044376
Zoeph123 <zoe.philipson2@gmail.com>
2026-01-07 20:04:44 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -3,30 +3,20 @@ import numpy as np
 from scipy.signal import convolve2d
 
 def load_image(path):
-    """
-    טוענת תמונה מהנתיב הנתון וממירה אותה לגווני אפור (Grayscale)
-    כדי שנוכל לבצע עליה חישובים מתמטיים בקלות.
-    """
+    # טעינת התמונה והמרתה לגווני אפור
     img = Image.open(path).convert('L')
     return np.array(img)
 
 def edge_detection(image):
-    """
-    מזהה קצוות בתמונה באמצעות שימוש בקרנל (Kernel) של Sobel.
-    """
-    kernel_x = np.array([[-1, 0, 1], 
-                         [-2, 0, 2], 
-                         [-1, 0, 1]])
+    # הגדרת קרנלים של Sobel
+    kernel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    kernel_y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
     
-    kernel_y = np.array([[-1, -2, -1], 
-                         [ 0,  0,  0], 
-                         [ 1,  2,  1]])
-    
+    # ביצוע קונבולוציה
     grad_x = convolve2d(image, kernel_x, mode='same')
     grad_y = convolve2d(image, kernel_y, mode='same')
     
+    # חישוב עוצמת הקצוות ונרמול
     edge_magnitude = np.sqrt(grad_x**2 + grad_y**2)
-    
     edge_magnitude = (edge_magnitude / edge_magnitude.max()) * 255
-    
     return edge_magnitude.astype(np.uint8)
